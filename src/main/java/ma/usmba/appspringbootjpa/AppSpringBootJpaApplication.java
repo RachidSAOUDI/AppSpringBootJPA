3aef3840ac8a712245e2c38865de243ec845c3f5
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2022-10-01 12:09:19 +0100

    initial commit

---
+++ src/main/java/ma/usmba/appspringbootjpa/AppSpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package ma.usmba.appspringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringBootJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSpringBootJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/usmba/appspringbootjpa/AppSpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.usmba.appspringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringBootJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
