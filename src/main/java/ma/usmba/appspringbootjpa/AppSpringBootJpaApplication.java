4c8e944eae950cc0fa3be6ba62fba7fe7e3099ae
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2022-10-01 13:13:33 +0100

    insert example test in database h2

--- src/main/java/ma/usmba/appspringbootjpa/AppSpringBootJpaApplication.java
+++ src/main/java/ma/usmba/appspringbootjpa/AppSpringBootJpaApplication.java
@@ -1,13 +1,28 @@
 package ma.usmba.appspringbootjpa;
 
+import ma.usmba.appspringbootjpa.entities.Patient;
+import ma.usmba.appspringbootjpa.repositories.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AppSpringBootJpaApplication {
+import java.util.Date;
 
+@SpringBootApplication
+public class AppSpringBootJpaApplication implements CommandLineRunner {
+    @Autowired //injection de dependence
+    private PatientRepository patientRepository;
     public static void main(String[] args) {
         SpringApplication.run(AppSpringBootJpaApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        patientRepository.save(new Patient(null, "Hassan", new Date(), true, 56));
+        patientRepository.save(new Patient(null, "Mohammed", new Date(), false, 97));
+        patientRepository.save(new Patient(null, "Imane", new Date(), false, 210));
+
+
+    }
 }
