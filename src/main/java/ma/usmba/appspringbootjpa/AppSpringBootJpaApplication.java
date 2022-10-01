8ec14477939a8db73c311ff416e3b17213f31b1d
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2022-10-01 13:30:21 +0100

    display liste+find patient in console

--- src/main/java/ma/usmba/appspringbootjpa/AppSpringBootJpaApplication.java
+++ src/main/java/ma/usmba/appspringbootjpa/AppSpringBootJpaApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Date;
+import java.util.List;
 
 @SpringBootApplication
 public class AppSpringBootJpaApplication implements CommandLineRunner {
@@ -22,7 +23,21 @@ public class AppSpringBootJpaApplication implements CommandLineRunner {
         patientRepository.save(new Patient(null, "Hassan", new Date(), true, 56));
         patientRepository.save(new Patient(null, "Mohammed", new Date(), false, 97));
         patientRepository.save(new Patient(null, "Imane", new Date(), false, 210));
-
+        List<Patient>  patiens = patientRepository.findAll();
+        patiens.forEach(p->{
+            System.out.println("============================");
+            System.out.println(p.getId());
+            System.out.println(p.getNom());
+            System.out.println(p.getScore());
+            System.out.println(p.getDateNaissance());
+            System.out.println(p.isMalade());
+        });
+        System.out.println("***********************************");
+        Patient patient = patientRepository.findById(1L).orElse(null);// L for type Long Or use "new Long(1)"
+        if (patient != null){
+            System.out.println(patient.getNom());
+            System.out.println(patient.isMalade());
+        }
 
     }
 }
